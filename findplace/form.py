9e0b33f5000df0d894bd31189dd28323e3549f78
Taras Motalo <tarasmotalo@gmail.com>
2020-07-12 16:49:13 +0300

    Third commit

--- findplace/form.py
+++ findplace/form.py
@@ -1,5 +1,6 @@
 from django import forms
 
+
 class PlaceForm(forms.Form):
     type_place = forms.CharField(max_length=10, help_text='Enter one type of institution, for example: cafe, police')
     address = forms.CharField(help_text='Enter the inital address, for example: "Івано-Франківськ, Мазепи 42"')
--- venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/belca88/Django/findapi/venv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
